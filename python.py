b4eb1f3b684d7ef1bf30c81cbf9f58502cdb266b
NSHIMYUMUREMYI Olivier <oliviernshimyumuremyi4@gmail.com>
2025-03-17 19:33:49 +0200

    version four

--- python.py
+++ python.py
@@ -30,3 +30,22 @@
 # print(f"List={k}")
 # for i in range(1,6):
 #      print(f"word :{i}= {len(k[i-1])}")
+
+#VERSION 4
+
+
+# print("KAJIPOTEFU SYSTEM\n")
+
+# n=str(input("Enter Text : "))
+# for A in n:
+#      b=n.replace("A","K")
+
+# for I in b:
+#      c=b.replace("I","J")
+# for O in c:
+#      d=c.replace("O","P")
+# for E in d:
+#      e=d.replace("E","T")
+# for U in e:
+#      m=e.replace("U","F")
+# print(m)
