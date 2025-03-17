12bdf05426f3b1af1cd8c05243d526d5710f332b
NSHIMYUMUREMYI Olivier <oliviernshimyumuremyi4@gmail.com>
2025-03-17 19:32:45 +0200

    versioni 3

--- python.py
+++ python.py
@@ -2,6 +2,31 @@
 #print(f"length:{len(a)}")
 
 
-a=str(input("Enter any text:"))
-b=a.replace(" ","")
-print(f"length:{len(b)}")
+#a=str(input("Enter any text:"))
+#b=a.replace(" ","")
+#print(f"length:{len(b)}")
+
+
+# print("YOU ARE GOING TO ENTER FIVE WORDS")
+# c=str(input("First word:"))
+# f=str(input("Second word:"))
+# g=str(input("Thrid word:"))
+# k=str(input("Fourth word:"))
+# l=str(input("Firth word:"))
+# z=c.replace(" ","")
+# y=f.replace(" ","")
+# x=g.replace(" ","")
+# u=k.replace(" ","")
+# v=l.replace(" ","")
+# print(f"word 1:{len(z)}\nword 2:{len(y)}\nword 3:{len(x)}\nword 4:{len(u)}\nword 5:{len(v)}")
+
+#second method
+
+#k=[]
+# for i in range(1,6):
+#     n=str(input(f"Enter word {i}: "))
+
+#     k.append(n)
+# print(f"List={k}")
+# for i in range(1,6):
+#      print(f"word :{i}= {len(k[i-1])}")
